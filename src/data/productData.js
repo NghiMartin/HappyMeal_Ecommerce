b03cb499c7f935b26a1dd9a39d801d4ad61fda1e
nghimartin <soobinnghi@gmail.com>
2024-10-18 22:02:21 +0700

    #1 first version

---
+++ src/data/productData.js
@@ -0,0 +1,112 @@
+const productData = [
+    {
+        "id": 1,
+    "title": "Salad Trứng",
+    "price": 39000,
+    "description": "Trứng gà luộc, rau củ quả(có thể thay đổi theo mùa), sốt ăn kèm.",
+    "category": "SALAD",
+    "image": "https://png.pngtree.com/png-vector/20240811/ourlarge/pngtree-wholesome-fresh-salad-with-mixed-veggies-png-image_13451249.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 2,
+    "title": "Salad Gà",
+    "price": 49000,
+    "description": "Ức gà nướng mật ong, rau củ quả(có thể thay đổi theo mùa), sốt ăn kèm.",
+    "category": "SALAD",
+    "image": "https://png.pngtree.com/png-clipart/20190826/ourlarge/pngtree-chicken-salad-png-image_1701085.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 3,
+    "title": "Salad Bò",
+    "price": 59000,
+    "description": "Bò sốt tiêu đen, rau củ quả(có thể thay đổi theo mùa), sốt ăn kèm.",
+    "category": "SALAD",
+    "image": "https://e7.pngegg.com/pngimages/28/896/png-clipart-roast-beef-pizza-coleslaw-salad-roasting-thai-chilli-food-beef.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 4,
+    "title": "Cơm Bò Sốt Tiêu Đen",
+    "price": 59000,
+    "description": "Bò sốt tiêu đen, rau củ quả, cơm gạo lứt, sốt ăn kèm.",
+    "category": "CƠM GẠO LỨT",
+    "image": "https://e7.pngegg.com/pngimages/598/606/png-clipart-teppanyaki-mole-sauce-american-chinese-cuisine-iron-black-pepper-bowl-of-beef-food-black-hair.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 5,
+    "title": "Cơm Gà Nướng Sốt Teriyaki",
+    "price": 49000,
+    "description": "Ức gà nướng sốt teriyaki, rau củ quả, cơm gạo lứt, sốt ăn kèm.",
+    "category": "CƠM GẠO LỨT",
+    "image": "https://png.pngtree.com/png-clipart/20201209/original/pngtree-teriyaki-chicken-rice-png-image_5648163.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 6,
+    "title": "Cơm Cá Hồi Sốt Chanh Dây",
+    "price": 69000,
+    "description": "Cá hồi áp chảo sốt chanh dây, rau củ quả, cơm gạo lứt, sốt ăn kèm.",
+    "category": "CƠM GẠO LỨT",
+    "image": "https://www.sugartown.vn/upload/sanpham/10cahoisotchanhdanh-17005561682.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 7,
+    "title": "Bún Bò Sốt Tiêu Đen",
+    "price": 59000,
+    "description": "Bò sốt tiêu đen, rau củ quả, bún gạo lứt, sốt ăn kèm.",
+    "category": "BÚN GẠO LỨT",
+    "image": "https://png.pngtree.com/png-vector/20240811/ourlarge/pngtree-wholesome-fresh-salad-with-mixed-veggies-png-image_13451249.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 8,
+    "title": "Bún Ức Gà Nướng Mật Ong",
+    "price": 49000,
+    "description": "Ức gà nướng mật ong, rau củ quả, bún gạo lứt, sốt ăn kèm.",
+    "category": "BÚN GẠO LỨT",
+    "image": "https://e7.pngegg.com/pngimages/993/674/png-clipart-teppanyaki-stew-black-pepper-meat-iron-sauce-black-pepper-beef-food-black-hair-thumbnail.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+    {
+        "id": 9,
+    "title": "Bún Cá Hồi Sốt Chanh Dây",
+    "price": 69000,
+    "description": "Cá hồi áp chảo sốt chanh dây, rau củ quả, bún gạo lứt, sốt ăn kèm.",
+    "category": "BÚN GẠO LỨT",
+    "image": "https://png.pngtree.com/png-vector/20240811/ourlarge/pngtree-wholesome-fresh-salad-with-mixed-veggies-png-image_13451249.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+    },
+]
+
+export { productData }
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,302 @@
+import React from "react";
+import { Footer, Navbar } from "../components";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+const Checkout = () => {
+  const state = useSelector((state) => state.handleCart);
+
+  const EmptyCart = () => {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 py-5 bg-light text-center">
+            <h4 className="p-3 display-5">Không có sản phẩm nào trong giỏ hàng</h4>
+            <Link to="/" className="btn btn-outline-dark mx-4">
+              <i className="fa fa-arrow-left"></i> Tiếp tục mua sắm
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const ShowCheckout = () => {
+    let subtotal = 0;
+    let shipping = 30000;
+    let totalItems = 0;
+    state.map((item) => {
+      return (subtotal += item.price * item.qty);
+    });
+
+    state.map((item) => {
+      return (totalItems += item.qty);
+    });
+    return (
+      <>
+        <div className="container py-5">
+          <div className="row my-4">
+            <div className="col-md-5 col-lg-4 order-md-last">
+              <div className="card mb-4">
+                <div className="card-header py-3 bg-light">
+                  <h5 className="mb-0">Tổng quan đơn hàng</h5>
+                </div>
+                <div className="card-body">
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                      Sản phẩm ({totalItems})<span>{Math.round(subtotal)} VNĐ</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center px-0">
+                      Phí vận chuyển 
+                      <span>{shipping} VNĐ</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                      <div>
+                        <strong>Tổng tiền</strong>
+                      </div>
+                      <span>
+                        <strong>{Math.round(subtotal + shipping)} VNĐ</strong>
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-7 col-lg-8">
+              <div className="card mb-4">
+                <div className="card-header py-3">
+                  <h4 className="mb-0">Địa chỉ thanh toán</h4>
+                </div>
+                <div className="card-body">
+                  <form className="needs-validation" novalidate>
+                    <div className="row g-3">
+                      <div className="col-sm-6 my-1">
+                        <label for="firstName" className="form-label">
+                        Tên
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="firstName"
+                          placeholder=""
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Valid first name is required.
+                        </div>
+                      </div>
+
+                      <div className="col-sm-6 my-1">
+                        <label for="lastName" className="form-label">
+                         Họ
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="lastName"
+                          placeholder=""
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Valid last name is required.
+                        </div>
+                      </div>
+
+                      <div className="col-12 my-1">
+                        <label for="email" className="form-label">
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="email"
+                          placeholder="you@example.com"
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Please enter a valid email address for shipping
+                          updates.
+                        </div>
+                      </div>
+
+                      <div className="col-12 my-1">
+                        <label for="address" className="form-label">
+                          Địa chỉ
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="address"
+                          placeholder="Số 1 Hải Phòng..."
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Please enter your shipping address.
+                        </div>
+                      </div>
+
+                      <div className="col-12">
+                        <label for="address2" className="form-label">
+                          Địa chỉ 2{" "}
+                          <span className="text-muted">(Không bắt buộc)</span>
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="address2"
+                          placeholder="Căn hộ hoặc văn phòng"
+                        />
+                      </div>
+
+                      <div className="col-md-5 my-1">
+                        <label for="country" className="form-label">
+                          Country
+                        </label>
+                        <br />
+                        <select className="form-select" id="country" required>
+                          <option value="">Chọn...</option>
+                          <option>Việt Nam</option>
+                          <option>America</option>
+                        </select>
+                        <div className="invalid-feedback">
+                          Please select a valid country.
+                        </div>
+                      </div>
+
+                      <div className="col-md-4 my-1">
+                        <label for="state" className="form-label">
+                          Thành phố
+                        </label>
+                        <br />
+                        <select className="form-select" id="state" required>
+                          <option value="">Chọn...</option>
+                          <option>Đà Nẵng</option>
+                          <option>Hồ Chí Minh</option>
+                          <option>Hà Nội</option>
+
+                        </select>
+                        <div className="invalid-feedback">
+                          Please provide a valid state.
+                        </div>
+                      </div>
+
+                      <div className="col-md-3 my-1">
+                        <label for="zip" className="form-label">
+                         Mã Vùng
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="zip"
+                          placeholder=""
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Zip code required.
+                        </div>
+                      </div>
+                    </div>
+
+                    <hr className="my-4" />
+
+                    <h4 className="mb-3">Thanh toán</h4>
+
+                    <div className="row gy-3">
+                      <div className="col-md-6">
+                        <label for="cc-name" className="form-label">
+                         Tên chủ thẻ
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="cc-name"
+                          placeholder=""
+                          required
+                        />
+                        <small className="text-muted">
+                          Full name as displayed on card
+                        </small>
+                        <div className="invalid-feedback">
+                          Name on card is required
+                        </div>
+                      </div>
+
+                      <div className="col-md-6">
+                        <label for="cc-number" className="form-label">
+                         Số thẻ tín dụng
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="cc-number"
+                          placeholder=""
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Credit card number is required
+                        </div>
+                      </div>
+
+                      <div className="col-md-3">
+                        <label for="cc-expiration" className="form-label">
+                          Hiệu lực
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="cc-expiration"
+                          placeholder=""
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Expiration date required
+                        </div>
+                      </div>
+
+                      <div className="col-md-3">
+                        <label for="cc-cvv" className="form-label">
+                          CVV
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="cc-cvv"
+                          placeholder=""
+                          required
+                        />
+                        <div className="invalid-feedback">
+                          Security code required
+                        </div>
+                      </div>
+                    </div>
+
+                    <hr className="my-4" />
+
+                    <button
+                      className="w-100 btn btn-success "
+                      type="submit" disabled
+                    >
+                      Tiếp tục thanh toán
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">Thanh toán</h1>
+        <hr />
+        {state.length ? <ShowCheckout /> : <EmptyCart />}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Checkout;
